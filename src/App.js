d4c8be0c63dfd3038905ae38138173bcb048bba5
Deepak <deepakaklecha2020@gmail.com>
2022-11-04 22:25:34 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [city, setCity] = useState("Mumbai");
+  const [search, setSearch] = useState("Mumbai");
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=9b1076dacaaa7f1a2c460a496258de89`;
+      const response = await fetch(url);
+      const resjson = await response.json();
+      console.log(resjson);
+      setCity(resjson.main);
+    };
+    fetchApi();
+  }, [search]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box">
+        <div className="inputbox">
+          <input
+            type="search"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+        </div>
+        {!city ? (
+          <p>No Data Found</p>
+        ) : (
+          <div>
+            <div className="info">
+              <div className="city">
+                <h2>{search}</h2>
+              </div>
+              <div className="temp">
+                <h3>{city.temp} °Cel</h3>
+              </div>
+              <div className="minmax">
+                <p>
+                  Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
